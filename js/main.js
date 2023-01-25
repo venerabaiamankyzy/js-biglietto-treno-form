0c2a0a7ef00f7a862b5ba6a0a7e3e1097c371200
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-01-25 14:34:29 +0100

    fixed

--- js/main.js
+++ js/main.js
@@ -10,23 +10,23 @@ userButtonGenera.addEventListener(
   'click',
 
   function () {
-      userKm = parseInt(userKm.value);
-      console.log("userKm", typeof userKm);
+    userKm = parseInt(userKm.value);
+    console.log("userKm", typeof userKm);
 
-      const ticketPrice = 0.21;
-      let price = (userKm * ticketPrice);
-      document.getElementById("costo biglietto").innerHTML = price;
+    const ticketPrice = 0.21;
+    let price = (userKm * ticketPrice);
+    document.getElementById("costo biglietto").innerHTML = price;
  
-      userAge = userAge.value;
+    userAge = userAge.value;
 
     if (userAge == "minorenne") {
-         //lo sconto per i minorenni
+      //lo sconto per i minorenni
       const discountUnder18 = 20 / 100;
       console.log(discountUnder18, typeof discountUnder18);
     
-         //il prezzo scontato
-         const discountAmountUnder18 = (price - (price * discountUnder18.toFixed(2)));
-         document.getElementById("costo biglietto").innerHTML = discountAmountUnder18;
+      //il prezzo scontato
+      const discountAmountUnder18 = (price - (price * discountUnder18.toFixed(2)));
+      document.getElementById("costo biglietto").innerHTML = discountAmountUnder18;
     } else if (userAge == "over") {
       //lo sconto per gli over 65
       const discountOver65 = 40 / 100;
@@ -34,29 +34,27 @@ userButtonGenera.addEventListener(
       //il prezzo scontato
       const discountAmountOver65 = (price - (price * discountOver65.toFixed(2)));
       document.getElementById("costo biglietto").innerHTML = discountAmountOver65;
-    } 
+    }
     document.getElementById("user name").innerHTML = userName.value;
     
-    let carrozza = Math.floor(Math.random () * 20);
+    let carrozza = Math.floor(Math.random() * 20);
     document.getElementById("carrozza").innerHTML = carrozza;
 
-    let codiceCp = Math.floor(Math.random () * 100000) + 1;
+    let codiceCp = Math.floor(Math.random() * 100000) + 1;
     document.getElementById("codice-cp").innerHTML = codiceCp;
 
-    let offerta ="nulla";
+    let offerta = "nulla";
     document.getElementById("offerta").innerHTML = (offerta);
 
     let discount = "prezzo pieno";
-    document.getElementById("sconto").innerHTML = discount; 
-    }
+    document.getElementById("sconto").innerHTML = discount;
+  }
+);
     
-)
 
 
 
 
 
 
-// if (isNaN(userKm) == true) {
-      //     alert("I valori inseriti non sono validi. Aggiornala pagina e riprova");
-      //   }
+
